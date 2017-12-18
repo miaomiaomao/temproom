bc45693b8c4b324bba34bc1f34898d07e5510c5b
hechao <366193530@qq.com>
2017-12-18 13:12:50 +0800

    修复Bug

--- send.py
+++ send.py
@@ -24,26 +24,26 @@ def client_connect():
     # print (bytes.decode(s.recv(1024)))
 
 def send(s,username):
-    while 1:
-        filepath = username+'.wav'
-        if os.path.isfile(filepath):
-            # 定义定义文件信息。
-            fileinfo_size = os.path.getsize(filepath)
+    # while 1:
+    filepath = username+'.wav'
+    if os.path.isfile(filepath):
+        # 定义定义文件信息。
+        fileinfo_size = os.path.getsize(filepath)
 
-            # 定义文件头信息，包含文件名和文件大小
-            #fhead = struct.pack('128sl', os.path.basename(filepath),os.stat(filepath).st_size)
-            s.send(str.encode(str(fileinfo_size)))
-            s.send(str.encode(username))
+        # 定义文件头信息，包含文件名和文件大小
+        #fhead = struct.pack('128sl', os.path.basename(filepath),os.stat(filepath).st_size)
+        s.send(str.encode(str(fileinfo_size)))
+        s.send(str.encode(username))
 
-            fp = open(filepath, 'rb')
-            while 1:
-                data = fp.read(1024)
-                if not data:
-                    print ('{0} file send over...'.format(filepath))
-                    break
-                s.send(data)
-        s.close()
-        break
+        fp = open(filepath, 'rb')
+        while 1:
+            data = fp.read(1024)
+            if not data:
+                print ('send over...')
+                break
+            s.send(data)
+    # s.close()
+
 
 def recv(s):
     buf = bytes.decode(s.recv(1024))
@@ -52,7 +52,8 @@ def recv(s):
     if buf:
         print('filesize is {0}'.format(buf))
         recvd_size = 0  # 定义已接收文件的大小
-        fp = open(username+'.wav', 'wb')
+        # fp = open(username+'.wav', 'wb')
+        fp = open('服务器接受.wav', 'wb')
         print('start receiving...')
 
         while not recvd_size == filesize:
@@ -63,6 +64,7 @@ def recv(s):
                 data = s.recv(filesize - recvd_size)
                 recvd_size = filesize
             fp.write(data)
+        print('done，接受到'+username)
         fp.close()
 
 
