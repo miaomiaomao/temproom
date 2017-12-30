8606949d0dc249d37ec2435dab1c13e83d76ade5
hchechao2 <366193530@qq.com>
2017-12-30 15:43:26 +0800

    u

--- server/server.py
+++ server/server.py
@@ -36,7 +36,7 @@ def check():
 
     for i in range(len(amountlist)):
         if amountlist[i] >=2:
-            time.sleep(0.5)
+            
             users=DataBase_server.curretroomusers(numberlist[i],cur)
             total.append((users,numberlist[i]))
     conn.close()
