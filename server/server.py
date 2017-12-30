343cd4210e184ea7efbd3be7dae21da9fa47aad6
hchechao2 <366193530@qq.com>
2017-12-30 16:21:43 +0800

    u

--- server/server.py
+++ server/server.py
@@ -33,13 +33,13 @@ def check():
     cur,conn=DataBase_server.ini()
     numberlist=DataBase_server.return_roomnumberlist(cur)
     amountlist=DataBase_server.return_useramountlist(cur)
-
+    conn.close()
     for i in range(len(amountlist)):
         if amountlist[i] >=2:
-
+            cur, conn = DataBase_server.ini()
             users=DataBase_server.curretroomusers(numberlist[i],cur)
             total.append((users,numberlist[i]))
-    conn.close()
+            conn.close()
     return total
 
 def trans1(client):
